3eacd777c5b8070ae454e2e8232ad0644708f19e
AshenFernando <ashenpavithra@gmail.com>
2026-03-21 13:46:09 +0530

    Initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,58 @@
+import { words } from "../constants"
+import Button from "./Button"
+
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+        <div className="absolute top-0 left-0 z-10">
+            <img src="/images/bg.png" alt="background"/>
+        </div>
+
+        <div className="hero-layout">
+
+            {/* Left- Hero Content */}
+            <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                <div className="flex flex-col gap-7">
+                    <div className="hero-text">
+                        <h1>Shaping 
+                            <span className="slide">
+                                <span className="wrapper">
+                                    {words.map((word)=> (
+                                        <span key={words.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                        <img
+                                            src={word.imgPath}
+                                            alt={word.text}
+                                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                                        <span>{word.text}</span>
+                                    </span>
+                                    ))}
+                                </span>
+
+                            </span>
+                        </h1>
+                        <h1>Into Real Projects</h1>
+                        <h1>That Deliver Results</h1>
+                    </div>
+                    <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                        Hi, I'm <b>Ashen</b>, a passionate full-stack developer with a knack for turning ideas into reality.
+                        . Let's collaborate to bring your vision to life and make a lasting impact in the digital world.
+                    </p>
+
+                    <Button
+                    className="md:w-80 md:h-16 w-60 h-12"
+                    id="button"
+                    text="See my Work"
+                    />
+
+                </div>
+            </header>
+            
+            {/* Right- 3D Model */}
+
+        </div>
+    </section>
+  )
+}
+
+export default Hero
